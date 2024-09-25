11b025a0cfce50897035612bdfc5c1d3ef130ba7
yeon44 <jangyeon33@gmail.com>
2024-09-25 22:13:41 +0900

    javascript_01

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var Name = "장연";
+console.log("안녕하세요. 만나서 반갑습니다. 저는", Name, "입니다.");
+document.write("안녕하세요. 만나서 반갑습니다. 저는", Name, "입니다.", "<br/>");
+
+var Year = 2021;
+document.write("올해의 연도는", Year, "입니다.", "<br/>");
+
+var snoopy_1 = "<img src ='./img/snoopy_01.png' alt='스누피 머리 위에 하트' ";
+document.write(snoopy_1, "<br/>");
+
+var snoopy_2 = "<div class = 'snoopy'> </div>";
+document.write(snoopy_2, "<br/>");
